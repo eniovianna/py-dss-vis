c1f35f3a38d63bff1e512c8b3fa3050c650a7217
Ênio Viana <eniocc@gmail.com>
2021-10-15 21:47:44 -0300

    Adding some files to support workflow and auto test

---
+++ tests/my_test.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+"""
+ Created by Ênio Viana at 15/10/2021 at 21:45:36
+ Project: py-dss-vis [out, 2021]
+"""
+import pytest
+
+
+@pytest.fixture
+def tes_00():
+    return True
